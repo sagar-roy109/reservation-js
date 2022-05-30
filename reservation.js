47a6e40e5550e38ade9d33df85a1ed6e2efb7b98
sagar-roy109 <sagar.roy.freelancer@gmail.com>
2022-05-29 21:09:52 -0400

    Final validation done

--- reservation.js
+++ reservation.js
@@ -1,6 +1,6 @@
 "use strict";
 
-$(document).ready( () => {
+$(document).ready(() => {
 	const emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/;
 
 
@@ -9,29 +9,118 @@ $(document).ready( () => {
 	$('#pickup_date').focus();
 
 
+	// error message
 
-
+	let err_msg = 'This field is required';
+	var err = false;
 	// submit form event
 
 	$('#submit').on('click', (e) => {
 
 		// get values
 		let date = $('#pickup_date').val().trim();
-		let days = $('#days').val().trim();
+		let days = +$('#days').val().trim();
 		let name = $('#name').val().trim();
 		let phone = $('#phone').val().trim();
 		let email = $('#email').val().trim();
 
+
+
+
 		//validation start
 
-		if(!date){
+		if (!days || !email || !phone || !name || !date) {
+
+			if (err === false) {
+				if (!date) {
+
+					e.preventDefault();
+
+					// create elemenet
+					const errEl = document.createElement('span');
+					errEl.classList.add('err-msg');
+					errEl.textContent = err_msg;
+
+					//append child
+					document.getElementById('pickup_date').insertAdjacentElement('afterend', errEl);
+
+				}
+				if (!days || !Number.isInteger(days)) {
+
+					e.preventDefault();
+
+					// create elemenet
+					const errEl = document.createElement('span');
+					errEl.classList.add('err-msg');
+					errEl.textContent = 'Please enter numaric number';
+
+					//append child
+					document.getElementById('days').insertAdjacentElement('afterend', errEl);
+
+				}
+				if (!name) {
+
+					e.preventDefault();
+					// create elemenet
+					const errEl = document.createElement('span');
+					errEl.classList.add('err-msg');
+					errEl.textContent = err_msg;
+
+					//append child
+					document.getElementById('name').insertAdjacentElement('afterend', errEl);
+
+
+
+				}
+				if (!phone) {
+
+					e.preventDefault();
+					// create elemenet
+					const errEl = document.createElement('span');
+					errEl.classList.add('err-msg');
+					errEl.textContent = err_msg;
+
+					//append child
+					document.getElementById('phone').insertAdjacentElement('afterend', errEl);
+
+
+
+				}
+				if (!email) {
+
+					e.preventDefault();
+					// create elemenet
+					const errEl = document.createElement('span');
+					errEl.classList.add('err-msg');
+					errEl.textContent = 'Please enter valid email';
+
+					//append child
+					document.getElementById('email').insertAdjacentElement('afterend', errEl);
+
+				} else if (!document.getElementById('email').value.match(emailPattern)) {
+
+					// create elemenet
+					const errEl = document.createElement('span');
+					errEl.classList.add('err-msg');
+					errEl.textContent = 'Please enter valid email';
+
+					//append child
+					document.getElementById('email').insertAdjacentElement('afterend', errEl);
+
+
+
+				}
+
+			}
+
 			e.preventDefault();
-			$('#pickup_date').siblings('<h2>test</h2>');
-			console.log('test');
 		}
 
+		// email validation
 
 
+
+		return err = true;
 
 
 	});
